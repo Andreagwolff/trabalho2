dd8c2cd22899065898738e4ace677489b9427d5f
Andre Wolff <andre.wolff@rede.ulbra.br>
2019-11-28 11:18:24 -0300

    Initial commit

---
+++ app/src/main/java/br/com/andre/trabalho/fileira.java
@@ -0,0 +1,13 @@
+package br.com.andre.trabalho;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class fileira extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fileira);
+    }
+}
